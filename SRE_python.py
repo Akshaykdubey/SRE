58f3189706d1a9c6946ac2564ad204da53f76ae0
Akshay <akshay.dubey@salesforce.com>
2019-08-30 18:35:04 +0100

    Changes

--- SRE_python.py
+++ SRE_python.py
@@ -1,2 +1,2 @@
 hex_value = input('Enter hex value: ').lstrip('#')
-print('RGB =', tuple(int(hex_value[i:i+2], 16) for i in (0, 2, 4)))
+print('RGB = ', tuple(int(hex_value[i:i+2], 16) for i in (0, 2, 4)))
